af9afdb0f8c78c4d9e59ac6e7b96559e0402a8a9
llu025 <luigi.t.luppino@uit.no>
2024-08-14 15:04:08 +0200

    Some changes I forgot to commit

--- utils.py
+++ utils.py
@@ -6,16 +6,22 @@ import traceback
 import SimpleITK as sitk
 import matplotlib.pyplot as plt
 import time
+import wandb
+import pydensecrf.densecrf as dcrf
+from pydensecrf.utils import create_pairwise_gaussian, create_pairwise_bilateral
+from scipy.ndimage import gaussian_filter
+from pdb import set_trace as bp
 
 
-def correct_proba(patient, probabilities, maximum_distance=20):
+def correct_proba(patient, probabilities, maximum_distance=15):
     """
     The probabilities calculated by the model can be corrected by this function,
     which finds the minimum distance between a voxel and the tumor/cavity, and
     attenuates the probabilities accordingly. The attenuating factor decays more
     or less quickly depending on the parameter <slope> of the hyperbolic tangent.
-    The steeper the slope, the faster the factor decreases. With <slope>=1, a
-    19mm (20mm, 21mm) distance corresponds to a factor of 0.88 (0.5, 0.12).
+    The steeper the slope, the faster the factor decreases. With <slope>=1 and
+    <maximum_distance>=20, distances of 19mm, 20mm, and 21mm correspond to factors
+    of 0.88, 0.5, and 0.12 respectively.
 
     Input:
         - patient: path to a patient folder. Needed to know the location of each
@@ -92,7 +98,7 @@ def create_3d_image(patient):
     return None
 
 
-def fuse_t1ce_and_proba(patient):
+def fuse_t1ce_and_proba(patient, proba=None, name=None):
 
     """
     This function creates the DICOM file with the calculated probabilities fused
@@ -108,10 +114,12 @@ def fuse_t1ce_and_proba(patient):
     # The probabilities are loaded from the Nifti image for convenience,
     # and their location saved in <mask>.The loaded probabilities are
     # grey levels (0 - 255), they have to be scaled back to 0 - 1.
-    path = os.path.join(patient, "saved_images", "probabilities.nii")
-    if not os.path.isfile(path):
-        create_3d_image(patient)
-    proba = nib.load(path).get_fdata()
+
+    if proba is None:
+        path = os.path.join(patient, "saved_images", "probabilities.nii")
+        if not os.path.isfile(path):
+            create_3d_image(patient)
+        proba = nib.load(path).get_fdata()
     mask = ~np.isnan(proba)
     proba = (proba - np.nanmin(proba)) / (np.nanmax(proba) - np.nanmin(proba))
 
@@ -150,17 +158,82 @@ def fuse_t1ce_and_proba(patient):
     for tag, value in new_tags:
         new_dicom.SetMetaData(tag, value)
     sitk.WriteImage(
-        new_dicom, os.path.join(patient, "saved_images", "t1ce_fused_proba.dcm")
+        new_dicom,
+        os.path.join(
+            patient,
+            "saved_images",
+            "t1ce_fused_proba" + ["", "_" + name][isinstance(name, str)] + ".dcm",
+        ),
     )
 
     return None
 
 
+def _dense_gaussian_filtering(probabilities):
+    """
+    Test
+    """
+    d = probabilities[..., np.newaxis]
+    d = np.concatenate((d, 1.0 - d), axis=3)
+    W, H, D = d.shape[0:3]
+
+    CD = dcrf.DenseCRF(W * H * D, 2)
+    d[d == 0] = 10e-20
+    U = -(np.log(d))
+    U = U.transpose(3, 0, 1, 2).reshape((2, -1))
+    U = U.copy(order="C")
+    CD.setUnaryEnergy(U.astype(np.float32))
+    scaling_dim = tuple(wandb.config["scaling_dim"] for i in range(3))
+    scaling_p = (wandb.config["scaling_p"],)
+    pairwise_energy_gaussian = create_pairwise_gaussian(scaling_dim, (W, H, D))
+    CD.addPairwiseEnergy(pairwise_energy_gaussian, compat=wandb.config["compat"])
+
+    # pairwise_energy_bilateral = create_pairwise_bilateral(
+    #     sdims=scaling_dim, schan=scaling_p, img=probabilities, chdim=-1
+    # )
+    # CD.addPairwiseEnergy(pairwise_energy_bilateral, compat=wandb.config["compat"])
+    Q = CD.inference(wandb.config["inference"])
+    heatmap = np.array(Q, dtype=np.float32)
+    heatmap = np.reshape(heatmap[0, ...], (W, H, D))
+    return heatmap
+
+
 if __name__ == "__main__":
     try:
-        path = "Patients"
-        fuse_t1ce_and_proba(os.path.join(path, "ntnu_20"))
+        wandb.init(
+            config={"compat": 3, "scaling_dim": 10, "scaling_p": 0.001, "inference": 5}
+        )
+        probs = nib.load("Patients/ntnu_34/saved_images/probabilities.nii").get_fdata()
+        bp()
+        probs[np.isnan(probs)] = 0
+        filtered_probs = _dense_gaussian_filtering(probs)
+        filtered_probs_save = np.copy(filtered_probs)
+        filtered_probs_save[np.isnan(probs)] = np.nan
+        fuse_t1ce_and_proba("Patients/ntnu_34", filtered_probs_save, "filtered")
+        Y = (
+            pd.read_parquet("Patients/ntnu_34/voxel_features_with_region.parquet")[
+                "region"
+            ]
+            == "rec"
+        )
 
+        from skimage.filters import threshold_otsu
+        from sklearn.metrics import roc_auc_score
+
+        filtered_probs = filtered_probs.ravel()[Y.index]
+        probs = probs.ravel()[Y.index]
+        print(np.mean(np.abs(probs - filtered_probs)))
+        auc = roc_auc_score(Y, filtered_probs)
+        original_auc = roc_auc_score(Y, probs)
+        print(auc - original_auc)
+        wandb.log({"gain": auc - original_auc})
+
+        corrected_proba = correct_proba("Patients/ntnu_34", probs)
+        print(np.mean(np.abs(probs - corrected_proba)))
+        corrected_auc = roc_auc_score(Y, corrected_proba)
+        print(corrected_auc - original_auc)
+
+        print(np.mean(np.abs(corrected_proba - filtered_probs)))
     except:
         traceback.print_exc()
         print("\a")
